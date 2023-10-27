14dfc6e0e9e1535f9047dc7250608fbe53a0ae96
Sasha <ivanpolarus2009@gmail.com>
2023-10-28 02:46:04 +0300

    feat: add active email modal

--- components/modals/auth/ActivationCode/index.tsx
+++ components/modals/auth/ActivationCode/index.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { Button } from '@/components/small/Button';
+import { CodeInput } from '@/components/small/CodeInput';
+import styles from './styles.module.css';
+import { authModalStore } from '../store';
+
+const { onCloseAuthModal } = authModalStore.getStore();
 
 export const ActivationCode = () => {
+  const [code, setCode] = useState<string>('');
+
   return (
-    <div />
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <p className={styles.title}>Welcome</p>
+        <Image
+          onClick={onCloseAuthModal}
+          className={styles.close}
+          src="/icons/close.svg"
+          alt="close"
+          width={24}
+          height={24}
+        />
+      </div>
+      <div className={styles.codeInputWrapper}>
+        <CodeInput code={code} setCode={setCode} codeLength={4} className={styles.codeInput} />
+      </div>
+      <Button className={styles.button}>
+        Continue
+      </Button>
+      <div className={styles.iconsWrapper}>
+        <p className={styles.iconTitle}>or</p>
+        <div className={styles.icons}>
+          <div className={styles.icon}>
+            <Image
+              src="/icons/google.svg"
+              alt="google"
+              width={20}
+              height={20}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
