5590db1684d5db1d99f9e10034f0c8ea9e28c5be
Sasha <ivanpolarus2009@gmail.com>
2023-11-22 08:52:54 +0200

    feat: add grid

--- components/hoc/ProfileLayout/ProfileLayout.tsx
+++ components/hoc/ProfileLayout/ProfileLayout.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
 
 import { ProfileHeader } from '@/components/large/ProfileHeader';
 import { ProfileTabs } from '@/components/large/ProfileTabs';
 import { ProfileLayoutProps } from './ProfileLayout.types';
+import styles from './profile-layout.module.css';
 
-export const ProfileLayout = ({ children, ...props }: ProfileLayoutProps) => {
+export const ProfileLayout = ({ children, className, ...props }: ProfileLayoutProps) => {
   return (
-    <div {...props}>
+    <div {...props} className={cn(styles.wrapper, className)}>
       <ProfileHeader />
-      <ProfileTabs />
-      {children}
+      <main className={styles.main}>
+        <ProfileTabs className={styles.tabs} />
+        {children}
+      </main>
     </div>
   );
 };
