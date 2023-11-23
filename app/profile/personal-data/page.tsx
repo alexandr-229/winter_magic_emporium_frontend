22d7aee75305320d4fc7b776aa6a877dfe97dee1
Sasha <ivanpolarus2009@gmail.com>
2023-11-23 06:49:46 +0200

    feat: add layout

--- app/profile/personal-data/page.tsx
+++ app/profile/personal-data/page.tsx
@@ -1,10 +1,23 @@
-import { profilePage } from '@/components/hoc/ProfileLayout';
+'use client';
+
 import React from 'react';
+
+import { profilePage } from '@/components/hoc/ProfileLayout';
+import { Input } from '@/components/small/Input';
+import { Button } from '@/components/small/Button';
+import styles from './styles.module.css';
 
 const PersonalData = () => {
   return (
     <div>
-      <p>Personal data</p>
+      <h2 className={styles.title}>Personal data</h2>
+      <div className={styles.inputs}>
+        <Input placeholder="Enter your name" className={styles.input} />
+        <Input placeholder="Enter your last name" className={styles.input} />
+        <Input placeholder="+38 (096) ___ __ __" className={styles.input} />
+        <Input placeholder="E-mail" className={styles.input} />
+      </div>
+      <Button className={styles.save}>Save</Button>
     </div>
   );
 };
