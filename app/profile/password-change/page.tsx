ed0dac4dcd5f56e5afb03634233d4f199de6d5f1
Sasha <ivanpolarus2009@gmail.com>
2023-11-25 02:51:19 +0200

    feat: add layout

--- app/profile/password-change/page.tsx
+++ app/profile/password-change/page.tsx
@@ -1,11 +1,29 @@
-import { profilePage } from '@/components/hoc/ProfileLayout';
+'use client';
+
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { profilePage } from '@/components/hoc/ProfileLayout';
+import { Input } from '@/components/small/Input';
+import { Button } from '@/components/small/Button';
+
+import styles from './styles.module.css';
 
 const OrderHistory = () => {
   return (
-    <div>
-      <p>Change password</p>
-    </div>
+    <>
+      <ToastContainer />
+      <div>
+        <h3 className={styles.title}>Change password</h3>
+        <div className={styles.inputs}>
+          <Input className={styles.input} placeholder="Enter old password" />
+          <Input className={styles.input} placeholder="Enter new password" />
+          <Input className={styles.input} placeholder="Confirm new password" />
+        </div>
+        <Button className={styles.button}>Save</Button>
+      </div>
+    </>
   );
 };
 
